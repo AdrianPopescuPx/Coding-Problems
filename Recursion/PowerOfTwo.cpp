d7331806d40c422311624525916dcce1de5bedaf
adrianpopescupx <adrianpopescupx@gmail.com>
2024-01-28 15:46:49 +0200

    PowerOfTwo

---
+++ Recursion/PowerOfTwo.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 1) {
+            return true;
+        }   else if (n % 2 != 0 || n <= 0)
+            return false;
+        return isPowerOfTwo(n / 2);
+    }
+};
+
+int main() {
+
+    return 0;
+}
